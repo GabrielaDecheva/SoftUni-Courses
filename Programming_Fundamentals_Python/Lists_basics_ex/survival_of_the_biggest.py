9f49dc0d8654611ff450689129d02ca9674723a3
GabrielaDecheva <gabriela.v.decheva@gmail.com>
2023-10-01 14:43:43 +0300

    update

--- Programming_Fundamentals_Python/Lists_basics_ex/survival_of_the_biggest.py
+++ Programming_Fundamentals_Python/Lists_basics_ex/survival_of_the_biggest.py
@@ -1,12 +1,12 @@
+import sys
+
 list_of_numbers = input().split()
 numbers_to_remove = int(input())
 list_of_integers = []
 for current_number in list_of_numbers:
     list_of_integers.append(int(current_number))
-list_of_integers.sort(reverse=True)
-while len(list_of_numbers) > (len(list_of_numbers) - numbers_to_remove):
-    list_of_integers.sort(reverse=True)
-    list_of_integers.pop()
-    numbers_to_remove -= 1
+for _ in range(numbers_to_remove):
+    smallest_number = min(list_of_integers)
+    list_of_integers.remove(smallest_number)
 
-print(list_of_integers)
+print(*list_of_integers, sep=', ')
