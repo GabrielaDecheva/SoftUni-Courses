8f56298281cb8498e84a6835a8151eedd5e70bcc
GabrielaDecheva <gabriela.v.decheva@gmail.com>
2023-10-20 22:50:44 +0300

    upgraded code

--- Programming_Fundamentals_Python/List_advanced_ex/softuni_course_planning.py
+++ Programming_Fundamentals_Python/List_advanced_ex/softuni_course_planning.py
@@ -1,3 +1,45 @@
+def add_func(current_lesson, schedule):
+    if lesson not in initial_schedule:
+        initial_schedule.append(lesson)
+
+
+def insert_func(current_lesson, current_index, schedule):
+    if lesson not in initial_schedule:
+        initial_schedule.insert(index, lesson)
+
+
+def remove_func(current_lesson, exercises, schedule):
+    if lesson in initial_schedule:
+        initial_schedule.remove(lesson)
+    if lesson in exercises:
+        exercises.remove(lesson)
+        initial_schedule.remove(f'{lesson}-Exercise')
+
+
+def swap_func(lesson1, lesson2, schedule, exercises):
+    if lesson1 in schedule and lesson2 in schedule:
+        first_index = schedule.index(lesson1)
+        second_index = schedule.index(lesson2)
+        schedule[first_index], schedule[second_index] = schedule[second_index], schedule[first_index]
+        if lesson1 in exercises:
+            schedule.pop(first_index + 1)
+            schedule.insert(second_index + 1, f'{lesson1}-Exercise')
+        if lesson2 in exercises:
+            schedule.pop(second_index + 1)
+            schedule.insert(first_index + 1, f'{lesson2}-Exercise')
+
+
+def exercise_func(current_lesson, schedule, exercises):
+    if current_lesson in schedule and current_lesson not in exercises:
+        lesson_index = schedule.index(current_lesson)
+        schedule.insert(lesson_index + 1, f'{current_lesson}-Exercise')
+        exercises.append(current_lesson)
+    elif current_lesson not in schedule:
+        schedule.append(current_lesson)
+        schedule.append(f'{current_lesson}-Exercise')
+        exercises.append(current_lesson)
+
+
 initial_schedule = input().split(', ')
 command = input().split(':')
 exercises = []
@@ -5,44 +47,22 @@ exercises = []
 while command[0] != 'course start':
     if command[0] == 'Add':
         lesson = command[1]
-        if lesson not in initial_schedule:
-            initial_schedule.append(lesson)
+        add_func(lesson, initial_schedule)
     elif command[0] == 'Insert':
         lesson = command[1]
         index = int(command[2])
-        if lesson not in initial_schedule:
-            initial_schedule.insert(index, lesson)
+        insert_func(lesson, index, initial_schedule)
     elif command[0] == 'Remove':
         lesson = command [1]
-        if lesson in initial_schedule:
-            initial_schedule.remove(lesson)
-        if lesson in exercises:
-            exercises.remove(lesson)
-            initial_schedule.remove(f'{lesson}-Exercise')
-
+        remove_func(lesson, exercises, initial_schedule)
     elif command[0] == 'Swap':
         first_lesson = command[1]
         second_lesson = command[2]
-        if first_lesson in initial_schedule and second_lesson in initial_schedule:
-            first_index = initial_schedule.index(first_lesson)
-            second_index = initial_schedule.index(second_lesson)
-            initial_schedule[first_index], initial_schedule[second_index] = initial_schedule[second_index], initial_schedule[first_index]
-            if first_lesson in exercises:
-                initial_schedule.pop(first_index + 1)
-                initial_schedule.insert(second_index + 1, f'{first_lesson}-Exercise')
-            if second_lesson in exercises:
-                initial_schedule.pop(second_index + 1)
-                initial_schedule.insert(first_index + 1, f'{second_lesson}-Exercise')
+        swap_func(first_lesson, second_lesson, initial_schedule, exercises)
     elif command[0] == 'Exercise':
         lesson = command[1]
-        if lesson in initial_schedule and lesson not in exercises:
-            lesson_index = initial_schedule.index(lesson)
-            initial_schedule.insert(lesson_index + 1, f'{lesson}-Exercise')
-            exercises.append(lesson)
-        elif lesson not in initial_schedule:
-            initial_schedule.append(lesson)
-            initial_schedule.append(f'{lesson}-Exercise')
-            exercises.append(lesson)
+        exercise_func(lesson, initial_schedule, exercises)
+
     command = input().split(':')
 
 for element in initial_schedule:
