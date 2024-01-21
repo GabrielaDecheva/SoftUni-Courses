8dc3df2dcd0da1f217a29f6f8e84e956ab64ded0
GabrielaDecheva <gabriela.v.decheva@gmail.com>
2024-01-21 15:53:56 +0200

    more tasks

---
+++ Programming_Advanced_Python/Multidimensional_lists_ex/bombs.py
@@ -0,0 +1,60 @@
+# n = int(input())
+#
+# matrix = [[int(x) for x in input().split()] for _ in range(n)]
+# coordinates = ((int(x) for x in c.split(","))for c in input().split())
+#
+# directions = (
+#     (-1, 0), # up
+#     (1, 0), #down
+#     (0, -1),# left
+#     (0, 1 ), # right
+#     (-1, 1), # up right
+#     (-1, -1), # up left
+#     (1, -1), # bottom left
+#     (1, 1), # bottom right
+#     (0, 0), # current -> important to be last
+# )
+#
+# for row, col in coordinates:
+#     if matrix[row][col] <= 0:
+#         continue
+#
+#     for x, y in directions:
+#         r, c = row + x, col + y
+#
+#         if 0 <= r < n and 0 <= c < n:
+#             matrix[r][c] -= matrix[row][col] if matrix[r][c] > 0 else 0
+#
+# alive_cells = [num for row in range(n) for num in (matrix[row]) if num > 0]
+#
+# print(f"Alive cells: {len(alive_cells)}")
+# print(f"Sum: {sum(alive_cells)}")
+#
+# [print(*row) for row in matrix]
+
+## Solution 2
+
+n = int(input())
+
+matrix = [[int(x) for x in input().split()] for _ in range(n)]
+coordinates = ((int(x) for x in c.split(","))for c in input().split())
+
+for row, col in coordinates:
+    if matrix[row][col] <= 0:
+        continue
+
+    bomb_value = matrix[row][col]
+
+    for x in range(-1, 2):
+        for y in range(-1, 2):
+            r, c = row + x, col + y
+
+        if 0 <= r < n and 0 <= c < n: # check if index is valid
+            matrix[r][c] -= bomb_value if matrix[r][c] > 0 else 0
+
+alive_cells = [num for row in range(n) for num in (matrix[row]) if num > 0]
+
+print(f"Alive cells: {len(alive_cells)}")
+print(f"Sum: {sum(alive_cells)}")
+
+[print(*row) for row in matrix]
