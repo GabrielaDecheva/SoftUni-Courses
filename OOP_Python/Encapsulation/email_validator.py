b8b57bf4f00f0081832269e060899c33be4ef5f3
GabrielaDecheva <gabriela.v.decheva@gmail.com>
2024-03-02 16:31:09 +0200

    more tasks

---
+++ OOP_Python/Encapsulation/email_validator.py
@@ -0,0 +1,29 @@
+class EmailValidator:
+
+    def __init__(self, min_length: int, mails: list, domains: list):
+        self.min_length = min_length
+        self.mails = mails
+        self.domains = domains
+
+    def __is_name_valid(self, name: str) -> bool:
+        return len(name) >= self.min_length
+
+    def __is_mail_valid(self, mail: str) -> bool:
+        return mail in self.mails
+
+    def __is_domain_valid(self, domain: str) -> bool:
+        return domain in self.domains
+
+    def validate(self, email: str) -> bool:
+        name, data = email.split("@")
+        mail, domain = data.split(".")
+        return self.__is_name_valid(name) and self.__is_mail_valid(mail) and self.__is_domain_valid(domain)
+
+
+mails = ["gmail", "softuni"]
+domains = ["com", "bg"]
+email_validator = EmailValidator(6, mails, domains)
+print(email_validator.validate("pe77er@gmail.com"))
+print(email_validator.validate("georgios@gmail.net"))
+print(email_validator.validate("stamatito@abv.net"))
+print(email_validator.validate("abv@softuni.bg"))
